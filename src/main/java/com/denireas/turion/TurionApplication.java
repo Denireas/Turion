fe7924adf98d07e841712b17d883c6bf9e1cab89
Denireas <samsonov311@yandex.ru>
2021-07-18 20:48:29 +0300

    Init

---
+++ src/main/java/com/denireas/turion/TurionApplication.java
@@ -0,0 +1,13 @@
+package com.denireas.turion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denireas/turion/TurionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denireas.turion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
