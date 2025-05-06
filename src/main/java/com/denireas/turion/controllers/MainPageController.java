dc48f73a3900da330109e814c6f9d6410bd41318
Denireas <samsonov311@yandex.ru>
2025-05-06 11:22:58 +0400

    log

--- src/main/java/com/denireas/turion/controllers/MainPageController.java
+++ src/main/java/com/denireas/turion/controllers/MainPageController.java
@@ -1,6 +1,7 @@
 package com.denireas.turion.controllers;
 
 import com.denireas.turion.services.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @RestController
+@Slf4j
 public class MainPageController {
 
     private final StudentService studentService;
@@ -22,7 +24,7 @@ public class MainPageController {
     public String mainPage() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
         String now = LocalDateTime.now().format(formatter);
-        System.out.println("\nСейчас: " + now);
+        log.info("\nСейчас: " + now);
         return "MainPage";
     }
 
